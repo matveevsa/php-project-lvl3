60a0f5e858b14ac18c93a65370b1c43d276d76b0
Sergey <matveevsa987@gmail.com>
2020-06-07 21:31:50 +0300

    fix

---
+++ app/Http/Controllers/DomainsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+
+class DomainsController extends Controller
+{
+    public function index()
+    {
+        $domains = DB::table('domains')->get();
+        return view('domains', compact('domains'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'domain.name' => 'required|url'
+        ]);
+
+        if ($validator->fails()) {
+            flash('Not a valid url')->error()->important();
+            return redirect()->route('domains.create');
+        }
+
+        $domainData = $request->input('domain');
+        $domainName = parse_url($domainData['name'], PHP_URL_HOST);
+
+        $currentId = DB::table('domains')
+            ->where('name', $domainName)
+            ->value('id');
+
+        if (!empty($currentId)) {
+            flash('Url already exists')->info()->important();
+            return redirect()->route('domains.show', ['id' => $currentId]);
+        }
+
+        $created_at = Carbon::now();
+        $domain = [
+            'name' => $domainName,
+            'created_at' => $created_at
+        ];
+
+        $newId = DB::table('domains')->insertGetId($domain);
+
+        flash('Url has been added')->info()->important();
+        return redirect()->route('domains.show', ['id' => $newId]);
+    }
+
+    public function show($id)
+    {
+        $domain = DB::table('domains')->find($id);
+
+        if (empty($domain)) {
+            return abort(404);
+        }
+        return view('show', compact('domain'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,52 +13,10 @@ use Carbon\Carbon;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', 'DomainsController@create')->name('domains.create');
 
-Route::get('/domains', function () {
-    $domains = DB::table('domains')->get();
-    return view('domains', ['domains' => $domains]);
-})->name('domains.index');
+Route::get('/domains', 'DomainsController@index')->name('domains.index');
 
-Route::post('/domains', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'domain.name' => 'required|url'
-    ]);
+Route::post('/domains', 'DomainsController@store')->name('domains.store');
 
-    if ($validator->fails()) {
-        flash('Not a valid url')->error()->important();
-        return redirect()->route('home');
-    }
-
-    $domainData = $request->input('domain');
-    $domainName = parse_url($domainData['name'], PHP_URL_HOST);
-
-    $currentId = DB::table('domains')
-        ->where('name', $domainName)
-        ->value('id');
-
-    if (!empty($currentId)) {
-        flash('Url already exists')->info()->important();
-        return redirect()->route('domains.show', ['id' => $currentId]);
-    }
-
-    $created_at = Carbon::now();
-    $domain = [
-        'name' => $domainName,
-        'created_at' => $created_at
-    ];
-
-    $newId = DB::table('domains')->insertGetId($domain);
-    flash('Url has been added')->info()->important();
-    return redirect()->route('domains.show', ['id' => $newId]);
-})->name('domains.store');
-
-Route::get('/domains/{id}', function($id) {
-    $domain = DB::table('domains')->find($id);
-    if (empty($domain)) {
-        return abort(404);
-    }
-    return view('show', compact('domain'));
-})->name('domains.show');
+Route::get('/domains/{id}', 'DomainsController@show')->name('domains.show');

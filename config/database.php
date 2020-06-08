ea7b59abdb71d01522e1599ab5387f12cf033fdd
Sergey <matveevsa987@gmail.com>
2020-06-08 21:15:48 +0300

    fix

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'driver' => 'pgsql',
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',

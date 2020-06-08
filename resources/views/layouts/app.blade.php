52ff7dc193655803bab154aa2eb23470414d2b8f
Sergey <matveevsa987@gmail.com>
2020-06-08 21:48:42 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
-   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>@yield('title', 'Main page')</title>
 </head>
 <body class="d-flex flex-column">

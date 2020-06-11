e0f54c881efc062842a5e04a41e9427d71912d85
Sergey <matveevsa987@gmail.com>
2020-06-11 11:00:33 +0300

    test fix

--- tests/Feature/DomainsControllerTest.php
+++ tests/Feature/DomainsControllerTest.php
@@ -2,36 +2,16 @@
 
 namespace Tests\Feature;
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Faker\Factory;
-use Illuminate\Support\Facades\DB;
 
 class DomainsControllerTest extends TestCase
 {
-    use RefreshDatabase;
-
-    private $id;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-
-    public function addDomain()
-    {
-        $faker = Factory::create();
-
-        $this->id = DB::table('domains')->insertGetId(
-            [
-                'name' => $faker->url,
-                'created_at' => $faker->dateTime()
-            ]
-        );
-    }
 
     public function testIndex()
     {
@@ -49,17 +29,15 @@ class DomainsControllerTest extends TestCase
 
     public function testStore()
     {
-        $this->addDomain();
+        $faker = Factory::create();
 
-        $this->assertDatabaseHas('domains', ['id' => $this->id]);
-    }
+        $data = [
+            'name' => $faker->url,
+            'created_at' => $faker->dateTime()
+        ];
 
-    public function testShow()
-    {
-        $this->addDomain();
-
-        $response = $this->get(route('domains.show', $this->id));
-
-        $response->assertStatus(200);
+        $response = $this->post(route('domains.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
     }
 }

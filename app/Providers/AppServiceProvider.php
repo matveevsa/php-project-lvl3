e802cbe7fb5d903fd73db4bfe5e21574a346bbbc
Sergey <matveevsa987@gmail.com>
2020-06-16 14:33:33 +0300

    fix assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function __construct()
+    {
+        // Force SSL in production
+        if (config('app.env') == 'production') {
+            URL::forceScheme('https');
+        }
+    }
     /**
      * Register any application services.
      *
